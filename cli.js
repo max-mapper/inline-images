559cf5f74c7486b7f9db232001d3c2fd90757e67
zunpiau <zunpiau@qq.com>
2018-07-06 01:04:40 +0800

    :sparkles: Introducing new features.
    Specify output file or directory
    Specify input file or directory
    Only process tag with specify class

--- cli.js
+++ cli.js
@@ -1,9 +1,24 @@
 #!/usr/bin/env node
 
-var fs = require('fs')
-var inliner = require('./')
+const fs = require('fs');
+const inliner = require('./');
+const program = require('commander');
 
-var html = fs.readFileSync(process.argv[2])
+program.usage('[options] <file|dir>')
+    .option('-c, --class <str>', 'Only process img tag whit the class')
+    .option('-o, --output <file>', 'Specify output file, use STDOUT by default')
+    .option('--output-dir <dir>', 'Specify an output directory')
+    .option('--root-dir <path>', 'Path used as root directory to resolve absolute includes')
+    .option('--remove-class', 'Remove class in --class options');
 
-var inlined = inliner(html)
-console.log(inlined.toString())
+program.parse(process.argv);
+
+if (program.args.length !== 1){
+  throw Error('require one directory or file')
+}
+const path = program.args[0];
+if(fs.statSync(path).isDirectory()){
+  inliner.dir(path, program.opts());
+} else {
+  inliner.file(path, program.opts());
+}
--- index.js
+++ index.js
@@ -12,30 +12,77 @@ const contentTypes = {
   ".webp": "image/webp"
 };
 
-module.exports = function(file) {
-  const base = path.dirname(file);
-  const dom = cheerio.load(fs.readFileSync(file).toString(), {decodeEntities: false});
-  inlineImages(dom);
-  
-  return new Buffer(dom.html({decodeEntities: false}));
-  
-  function inlineImages(dom) {
-    dom('img').each(function(idx, el) {
-      el = dom(el);
-      const src = el.attr('src');
-      console.info(src);
-      if (src && isLocal(src) && !path.isAbsolute(src)) {
-        const file = path.join(base, src);
-        const img = fs.readFileSync(file);
-        const contentType = contentTypes[path.extname(file)];
-        if (contentType == null) return;
-        const dataUri = "data:" + contentType + ";base64," + img.toString("base64");
-        el.attr('src', dataUri)
-      }
-    })
-  }
-  
-  function isLocal(href) {
-    return !url.parse(href).hostname;
+module.exports.file = function (file, options) {
+  const inlined = inlineImages(file, options);
+  if (options.output) {
+    fs.writeFileSync(options.output, inlined);
+  } else if (options.outputDir) {
+    fs.writeFileSync(path.join(options.outputDir, path.basename(file)), inlined);
+  } else {
+    console.log(inlined);
   }
 };
+
+module.exports.dir = function (dir, options) {
+  if (!options.outputDir) {
+    throw Error('require outdir option if args is a directory')
+  }
+  if (!fs.statSync(options.outputDir).isDirectory()) {
+    throw Error('outdir should be a directory')
+  }
+  const files = fs.readdirSync(dir).filter(file => path.extname(file) === '.html');
+  for (const file of files) {
+    const fileName = path.basename(file);
+    const inlined = inlineImages(path.join(dir, file), options);
+    const output = path.join(options.outputDir, fileName);
+    fs.writeFileSync(output, inlined);
+  }
+};
+
+function inlineImages(htmlFile, options) {
+  const selector = createSelector(options.class);
+  const rootDir = options.rootDir;
+  const relativeDir = path.dirname(htmlFile);
+  const dom = cheerio.load(fs.readFileSync(htmlFile).toString(), {decodeEntities: false});
+  dom(selector).each(function (index, element) {
+    element = dom(element);
+    const src = element.attr('src');
+    if (src && isLocal(src)) {
+      let imgPath;
+      if (path.isAbsolute(src)) {
+        if (rootDir) {
+          imgPath = path.join(rootDir, src);
+        } else {
+          console.error("skip img '%s' because of --root-dir option unset", src);
+          return;
+        }
+      } else {
+        imgPath = path.join(relativeDir, src);
+      }
+      const contentType = contentTypes[path.extname(imgPath)];
+      if (contentType == null) {
+        console.error("skip img '%s' because of file type unsupported", src);
+        return;
+      }
+      const img = fs.readFileSync(imgPath);
+      const dataUri = "data:" + contentType + ";base64," + img.toString("base64");
+      element.attr('src', dataUri);
+      if (options.class && options.removeClass) {
+        const classes = element.attr('class').split(/\s+/);
+        if (classes.length === 1)
+          element.removeAttr('class');
+        else
+          element.removeClass(options.class);
+      }
+    }
+  });
+  return dom.html({decodeEntities: false});
+}
+
+function createSelector(className) {
+  return className ? 'img.' + className : 'img';
+}
+
+function isLocal(href) {
+  return !url.parse(href).hostname;
+}
